e51f16fe2c7b5bfad7e92b66a6e3355e44185062
MauriceHunter <mauriceh1995@yahoo.com>
2019-04-16 13:40:22 -0400

    update

--- Lesson11/index.js
+++ Lesson11/index.js
@@ -1,5 +1,14 @@
-const moment = require('request');
+const request = require('request');
 const moment = require('moment');
+const rp = require('request-promise');
 
-
-console.log("his hair, WACK!");
+setInterval(() => {
+    rp.get('http://jsonplaceholder.typicode.com/posts/1',(err,response,body) => {
+        console.log(moment().format('h:mm:ss a'));
+        //process(response);
+    }).then( (success) => {
+        console.log(success);
+    }, (fail) => {
+        console.log(fail);
+    })
+},3000);
